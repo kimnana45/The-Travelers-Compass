3a6b6a0ebf478ce253aa45016f91d15aac4814ff
kimnana45 <kimle.lek@gmail.com>
2020-09-01 00:08:34 -0400

    added Budget GlobalContext

--- client/src/utils/BudgetGlobalState.js
+++ client/src/utils/BudgetGlobalState.js
@@ -1,5 +1,34 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useReducer } from "react";
+import { ADD_TRANSACTION, REMOVE_TRANSACTION, GET_TRANSACTIONS, TRANSACTION_ERROR } from "./actions";
+import axios from 'axios';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case GET_TRANSACTIONS:
+            return {
+                ...state,
+                transactions: action.payload,
+                loading: false
+            }
+        case REMOVE_TRANSACTION:
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction._id !== action.payload)
+            }
+        case ADD_TRANSACTION:
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload]
+            }
+        case TRANSACTION_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+}
 
 const initialState = {
     transactions: [],
@@ -9,8 +38,68 @@ const initialState = {
 
 export const BudgetGlobalContext = createContext(initialState);
 export const BudgetGlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer();
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     //Actions
-  
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/api/transactions');
+            dispatch({
+                type: GET_TRANSACTIONS,
+                payload: res.data.data
+            });
+        } catch (err) {
+            dispatch({
+                type: TRANSACTION_ERROR,
+                payload: err.response.data.error
+            });
+        }
+    }
+
+    async function deleteTransaction(id) {
+        try {
+            await axios.delete(`/api/transactions/${id}`);
+            dispatch({
+                type: REMOVE_TRANSACTION,
+                payload: id
+            });
+        } catch (err) {
+            dispatch({
+                type: TRANSACTION_ERROR,
+                payload: err.response.data.error
+            })
+        }
+    }
+
+    async function addTransaction(transaction) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/transactions/', transaction, config);
+            dispatch({
+                type: ADD_TRANSACTION,
+                payload: res.data.data
+            });
+        } catch (err) {
+            dispatch({
+                type: TRANSACTION_ERROR,
+                payload: err.response.data.error
+            })
+        }
+    }
+
+    return (<BudgetGlobalContext.Provider value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction
+    }}
+    >
+        {children}
+    </BudgetGlobalContext.Provider>)
 }
