6d48e2b30bdc4349f24369ae7c0cc2481f770e77
kimnana45 <kimle.lek@gmail.com>
2020-09-06 15:43:17 -0400

    deployment

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'built', 'index.html'))
+})
 // Add routes, both API and view
 app.use(routes);
 // app.use(apiRoutes);
