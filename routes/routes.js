297cbc4caa405161ac5ab94cc8b5b7531df29dc5
kimnana45 <kimle.lek@gmail.com>
2020-09-06 16:41:46 -0400

    deployment

--- routes/routes.js
+++ routes/routes.js
@@ -11,10 +11,10 @@ router.post('/api/register', function (req, res) {
 	//Do password validation here before attempting to register user, such as checking for password length, capital letters, special characters, etc.
 	const { firstName, lastName, username, email, profilePic, password } = req.body;
 	db.User.register(
-		new db.User({ 
-			firstName: firstName, 
-			lastName: lastName, 
-			username: username, 
+		new db.User({
+			firstName: firstName,
+			lastName: lastName,
+			username: username,
 			email: email,
 			profilePic: profilePic
 		}), password,
@@ -109,10 +109,10 @@ router.get('/api/trip/:id', (req, res) => {
 // Delete trip by trip Id & remove trip Id's from users
 router.delete('/api/trip/:id', (req, res) => {
 	let { id } = req.params;
-	db.Trip.findByIdAndRemove({ _id: id})
+	db.Trip.findByIdAndRemove({ _id: id })
 		.then(dbTrip => db.User.updateMany(
-			{ _id: { $in: dbTrip.travelers } }, 
-			{ $pull: { trips: { $in: id } } }, 
+			{ _id: { $in: dbTrip.travelers } },
+			{ $pull: { trips: { $in: id } } },
 			{ new: true }))
 		.then(dbTrip => res.json(dbTrip))
 });
@@ -137,26 +137,29 @@ router.put('/api/jointrip', (req, res) => {
 router.put('/api/trip', (req, res) => {
 	let { userId, tripId } = req.body;
 	db.Trip.findOneAndUpdate(
-		{ _id: tripId }, 
-		{ $pull: { travelers: { $in: userId } } }, 
+		{ _id: tripId },
+		{ $pull: { travelers: { $in: userId } } },
 		{ new: true })
 		.then(({ _id }) => db.User.updateOne(
-			{ _id: userId }, 
+			{ _id: userId },
 			{ $pull: { trips: { $in: tripId } } },
-			{ new: true }))		
+			{ new: true }))
 		.then(dbTrip => res.json(dbTrip))
-		.catch(err => res.status(422).json(err));	
+		.catch(err => res.status(422).json(err));
 });
 //Route to save emergency contact info
 router.put('/api/trip/emergencyContact', function (req, res) {
 	const { id, name, number } = req.body;
 	db.User.findOneAndUpdate(
-		{ _id: id }, 
-		{ $set: 
-			{ emergencyContact: { 
-				name: name,
-				number: number 
-			}}
+		{ _id: id },
+		{
+			$set:
+			{
+				emergencyContact: {
+					name: name,
+					number: number
+				}
+			}
 		},
 		{ new: true })
 		.then(dbUser => res.json(dbUser))
@@ -167,7 +170,7 @@ router.put('/api/trip/:id/flight', function (req, res) {
 	const { body } = req;
 	const { id } = req.params;
 	db.Trip.findOneAndUpdate(
-		{ _id: id }, 
+		{ _id: id },
 		{ $push: { flights: body } },
 		{ new: true })
 		.then(dbTrip => res.json(dbTrip.flights))
@@ -185,7 +188,7 @@ router.put('/api/trip/:id/lodging', function (req, res) {
 router.put('/api/trip/:tripId/lodging/:lodgingId', function (req, res) {
 	const { tripId, lodgingId } = req.params;
 	console.log(req.params)
-	db.Trip.findByIdAndUpdate({ _id: tripId}, { $pull : { lodging: { _id: lodgingId } } }, { new: true } )
+	db.Trip.findByIdAndUpdate({ _id: tripId }, { $pull: { lodging: { _id: lodgingId } } }, { new: true })
 		.then(dbTrip => res.json(dbTrip))
 		.catch(err => console.log(err));
 });
@@ -197,9 +200,10 @@ router.put('/api/trip/:id', function (req, res) {
 	const { id } = req.params;
 	const { body } = req;
 	db.Trip.findOneAndUpdate(
-		{ _id: id }, 
-		{ $push: 
-			{ pictures: body }
+		{ _id: id },
+		{
+			$push:
+				{ pictures: body }
 		},
 		{ new: true })
 		.then(dbUser => res.json(dbUser))
@@ -210,25 +214,25 @@ router.put('/api/trip/picture/:picId', function (req, res) {
 	const { picId } = req.params;
 	const { tripId } = req.body;
 	db.Trip.update(
-		{ _id: tripId }, 
-		{ $pull: { pictures: { _id: picId } } }, 
+		{ _id: tripId },
+		{ $pull: { pictures: { _id: picId } } },
 		{ new: true })
 		.then(dbTrip => res.json(dbTrip))
-		.catch(err => res.status(422).json(err));	
+		.catch(err => res.status(422).json(err));
 });
 
 
 //IDEAS ROUTES
 //route to add the idea
 router.put('/api/trip/:id/ideas', function (req, res) {
-	db.Trip.findByIdAndUpdate({ _id: req.params.id }, { $push : { toDos: req.body } }, { new: true } )
+	db.Trip.findByIdAndUpdate({ _id: req.params.id }, { $push: { toDos: req.body } }, { new: true })
 		.then(dbTrip => res.json(dbTrip.toDos))
 		.catch(err => console.log(err));
 });
 
 //route to delete the idea 
 router.put('/api/trip/:tripId/ideas/:ideaId', function (req, res) {
-	db.Trip.findByIdAndUpdate({ _id: req.params.tripId}, { $pull : { toDos: { _id: req.params.ideaId } } }, { new: true } )
+	db.Trip.findByIdAndUpdate({ _id: req.params.tripId }, { $pull: { toDos: { _id: req.params.ideaId } } }, { new: true })
 		.then(dbTrip => res.json(dbTrip))
 		.catch(err => console.log(err));
 });
@@ -237,16 +241,20 @@ router.put('/api/trip/:tripId/ideas/:ideaId', function (req, res) {
 //BUDGET & TRANSACTION ROUTES
 //route to add transaction
 router.put('/api/trip/:id/transaction', function (req, res) {
-	db.Trip.findByIdAndUpdate({ _id: req.params.id }, { $push : { expenses: req.body } }, { new: true } )
-		.then(({expenses}) => res.json(expenses[expenses.length - 1]))
+	db.Trip.findByIdAndUpdate({ _id: req.params.id }, { $push: { expenses: req.body } }, { new: true })
+		.then(({ expenses }) => res.json(expenses[expenses.length - 1]))
 		.catch(err => console.log(err));
 });
 //route to remove a transaction
 router.put('/api/trip/:tripId/transaction/:transactionId', function (req, res) {
 	console.log(req.params)
-	db.Trip.findByIdAndUpdate({ _id: req.params.tripId}, { $pull : { expenses: { _id: req.params.transactionId } } }, { new: true } )
+	db.Trip.findByIdAndUpdate({ _id: req.params.tripId }, { $pull: { expenses: { _id: req.params.transactionId } } }, { new: true })
 		.then(dbTrip => res.json(dbTrip))
 		.catch(err => console.log(err));
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'built', 'index.html'))
 });
 
 module.exports = router;
