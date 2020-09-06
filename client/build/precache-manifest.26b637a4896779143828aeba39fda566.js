0ea2fbec9d4729bc50bd194b1caaf15aae27daef
kimnana45 <kimle.lek@gmail.com>
2020-09-06 15:48:20 -0400

    deployment

---
+++ client/build/precache-manifest.26b637a4896779143828aeba39fda566.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2bcef7c230ac2cd81782e6eb8f7acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d5c824dc15c4ccf8bf",
+    "url": "/static/css/main.a08884aa.chunk.css"
+  },
+  {
+    "revision": "2e0bebf9b8c897c4aeab",
+    "url": "/static/js/2.b65a8854.chunk.js"
+  },
+  {
+    "revision": "bfa1854a2c0e869703bd60b082518e22",
+    "url": "/static/js/2.b65a8854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d5c824dc15c4ccf8bf",
+    "url": "/static/js/main.215d0dea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "38a13f50749dde07971e25832d81c233",
+    "url": "/static/media/header.38a13f50.png"
+  },
+  {
+    "revision": "a1a8b6ac84f2a3a4a9d7545676065e34",
+    "url": "/static/media/logo2.a1a8b6ac.png"
+  },
+  {
+    "revision": "11ec280066ccbac788ef02e363ce52f3",
+    "url": "/static/media/mern.11ec2800.png"
+  }
+]);
